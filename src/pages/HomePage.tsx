8c633b833d1cb4b18373f6cc7e2b57aa0711a285
sgyriley <gayoungshim@ewhain.net>
2022-04-06 10:01:16 +0900

    committed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+<div className="m-4">
+  <div className="text-3xl font-bold mb-5">New</div>
+  <div className="box-border h-96 w-5/6 p-5
+  border border-gray-300 bg-gray-300 text-left text-xl font-bold">아이스크림 맛집
+  
+  <div className='text-base text-left font-bold mt-3'>구경해보세요</div>
+  
+</div>
+
+ <div className="text-3xl font-bold my-5">List</div>
+  <div className="box-border h-96 w-5/6 p-6 border border-gray-300">
+    <div className="box-border h-1/2 w-full border-gray-300 bg-gray-200"></div>
+    <div className="box-border h-1/2 w-full border-gray text-left text-lg font-bold mb-3 mt-3">Card title
+    
+    <div className='mt-2 text-base text-left 
+    font-normal text-gray-400'>Sed vel turpis adipiscing penatibus orci 
+    neque. Erat sed fermentum ipsum vel quis 
+    quam. Nunc etiam dui tortor, non in 
+    aliquam laicinia tempor.</div>
+    <div className='flex felx-row '>
+
+       <button className='w-20 h-9 bg-slate-800 mt-3 mr-3 text-white text-sm font-normal'>button</button>
+       <button className='w-20 h-9 bg-slate-800 mt-3 text-white text-sm font-normal'>button</button>
+
+    </div>
+    
+    </div>
+
+  </div>
+
+  </div>
+
+
+  
+
+
+
+  );
 };
